82f14cbd943eb783222a06ab9dc2c59027a20643
tesfaye <contactesfaye>
2023-03-01 22:10:07 +0300

    project file updated

---
+++ index.js
@@ -0,0 +1,27 @@
+import { Book, UI, Storage } from './modules/functions.js';
+
+import './modules/elements.js';
+
+import './modules/time.js';
+
+document.addEventListener('DOMContentLoaded', UI.displayBooks);
+
+
+document.querySelector('#app-form').addEventListener('submit', (e) => {
+    e.preventDefault();
+    const title = document.querySelector('#title').value;
+    const author = document.querySelector('#author').value;
+    const id = Math.random();
+    const book = new Book(title, author, id);
+
+    UI.addBooks(book);
+
+    Storage.addToStore(book)
+
+    UI.clearValues();
+})
+
+document.querySelector('.books').addEventListener('click', (e) => {
+    UI.deleteBook(e.target);
+    Storage.removeBook(e.target.getAttribute('data-ref'));
+})
